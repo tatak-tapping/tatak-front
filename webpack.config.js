0e13c3423433765ac8a0efecf62ad90fd7a966e5
HongEunbeen <vvvv980@gmail.com>
2022-04-26 21:17:25 +0900

    chore : svg 사용을 위한 설정 변경

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    historyApiFallback: true,
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new Dotenv(),
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: 'public' }],
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.(ts|js)x?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }],
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
