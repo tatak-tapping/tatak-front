7890653be0103966cbea795de28d1f06042617c8
HongEunbeen <vvvv980@gmail.com>
2022-05-05 15:58:24 +0900

    feat : bubble menu 구현

---
+++ src/components/atoms/bubble/Bubble.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { BASE } from 'styles/colors';
+
+const Bubble = styled.div<{ 
+  isVisible: boolean,
+  width: string; 
+  height:string; 
+  padding?: string;
+  margin?:string;
+  right?:string;
+}>`
+  visibility: ${(props) => !props.isVisible && 'hidden'};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  padding: ${(props) => props.padding};
+  right: ${(props) => props.right};
+  position: absolute;
+  box-shadow: 0px 0px 2px 0px #00000040;
+  background-color: ${BASE[3]};
+  *{
+    background-color: ${BASE[3]};
+  };
+  ${(props) =>
+    props.isVisible &&
+    `
+    visibility: visible;
+    transform: translate3d(0,10px,0);
+    transition: all .2s ease-in-out;
+  `}
+`;
+
+export default Bubble;
---
+++ src/components/organisms/bubbles/UserProfileBubbleContent.tsx
@@ -0,0 +1,50 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import Bubble from "components/atoms/bubble/Bubble";
+import UserMenuButton from "components/molecules/profile/UserMenuButton";
+import UserProfileEdit from "components/molecules/profile/UserProfilEdit";
+import { useEffect, useRef } from "react";
+import { Box, Flex, Link } from "rebass";
+import { BASE, GRAY, PRIMARY } from "styles/colors";
+
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+interface UserProfileBubbleProp  {
+  isVisible: boolean;
+  onClose: VoidFunction;
+}
+
+const UserProfileBubbleContent =({ isVisible, onClose }: UserProfileBubbleProp) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(e.target as Node)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (isVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }
+  }, [isVisible]);
+
+  return(
+    <Wrapper ref={wrapperRef}>
+      <Bubble width="200px" height="320px" right="16px" 
+        padding="0 16px"
+        isVisible={isVisible}>
+        <UserProfileEdit />
+        <UserMenuButton />
+      </Bubble>
+    </Wrapper>
+  );
+};
+export default UserProfileBubbleContent;
