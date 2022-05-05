236499fba8bf83b0c974a0eaeb1747e9a834e60b
HongEunbeen <vvvv980@gmail.com>
2022-05-05 17:00:48 +0900

    feat : input, inputBase 생성 - input, icon, desc를 위해 inputBase로 분리

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -2,51 +2,55 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import { useRef } from "react";
-import { BASE, GRAY, PRIMARY } from "styles/colors";
+import { Box } from "rebass";
+import { BASE, ERROR, GRAY, PRIMARY } from "styles/colors";
 import InputBase from "./InputBase";
 
-interface InputProps  {
-  disabled?: boolean
+type InputProps = {
+  error?: boolean
   comment?: string
   icon?: React.ReactNode
-};
+} & React.InputHTMLAttributes<HTMLInputElement>
 
-const wrapper = (error?: boolean) => css`
-  display: flex;
-  cursor: text;
-`
-
-const style = css`
+const StyledInput = styled.input` 
+  width: 320px;
+  height: 40px;
   border: none;
   outline: none;
-  color: inherit;
-  background: none;
-  font-size: inherit;
+  background-color: ${BASE[3]};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 0px;
+  color:${GRAY[1]};
+  line-height: 150%;
   &::placeholder {
-    color: black;
+    color: ${GRAY[6]};
     opacity: 0.5;
-  }
-  &:disabled {
-    cursor: not-allowed;
-    color: inherit;
-  }
-`
-const Input = ({ comment, icon, disabled = false, ...rest }: InputProps) => {
-  const ref = useRef<HTMLInputElement>(null);
+  };
+`;
 
+
+const Wrapper = styled.div<{error: boolean}>`
+  display: flex;
+  border-bottom: ${(props) => props.error ? `1px solid ${ERROR}` : `1px solid ${GRAY[6]}`};
+  &input:focus{
+    color:${GRAY[1]};
+  }
+`;
+
+const Input = ({ comment, icon, error = false, ...rest }: InputProps) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
     <InputBase
-      css={wrapper(!!comment)}
-      disabled={disabled}
       comment={comment}
-      icon={icon}
-    >
-      <input
-        css={style}
-        ref={ref}
-        disabled={disabled}
-        {...rest}
-      />
+      error={error}>
+      <Wrapper error={error}>
+        <StyledInput
+          ref={ref}
+          {...rest}/>
+        {icon}
+      </Wrapper>
     </InputBase>
   );
 };
--- src/components/atoms/input/InputBase.tsx
+++ src/components/atoms/input/InputBase.tsx
@@ -7,33 +7,15 @@ import { BASE, GRAY, PRIMARY } from "styles/colors";
 
 interface InputBaseProps {
   className?: string
-  style?: React.CSSProperties
-  children?: React.ReactNode
-  disabled?: boolean
+  error? :boolean
   comment?: string
-  icon?:React.ReactNode
+  children?: React.ReactNode
 }
-
-
-const StyledInput = (disabled: boolean) => css`
-  display: flex;
-  outline: none;
-  min-height: 40px;
-  max-height: 62px;
-  width: 348px; 
-  /* ${disabled &&
-  css`
-    opacity: 0.4;
-    cursor: not-allowed;
-  `}; */
-`
-
 const InputBase = ({
   className,
-  style,
   children,
-  disabled = false,
-  comment = "dd",
+  error,
+  comment,
   ...rest
 }: InputBaseProps, ref: React.Ref<HTMLDivElement>) => {
   return (
@@ -41,19 +23,13 @@ const InputBase = ({
       <div
         className={className}
         ref={ref}
-        css={StyledInput(disabled)}
-        style={style}
         {...rest}
-        >
+      >
         {children}
       </div>
-      {comment && (
-        <Text as="p" style={{ color: 'red' }}>
-          {comment}
-        </Text>
-      )}
+      {comment && (<Text as="p">{comment}</Text>)}
     </Box>
   );
 };
 
-export default InputBase;
+export default forwardRef<HTMLDivElement, InputBaseProps>(InputBase)
