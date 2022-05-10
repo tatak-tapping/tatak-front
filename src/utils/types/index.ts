1448740d70b93b37b204fc39bd396887a6f9996a
HongEunbeen <vvvv980@gmail.com>
2022-05-10 22:36:40 +0900

    feat : interface 설계 및 atom 설정

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -6,3 +6,72 @@ export interface IUser{
   accessToken:string;
   refreshToken:string;
 }
+
+export interface IAuthor{
+  id: number;
+  email: string;
+  nickname: string;
+  profileImageUrl: string;
+}
+
+export interface ICategory{
+  categoryCode: number;
+  categoryName: string;
+}
+
+export interface ITopic{
+  topicCode: number;
+  topicName: string;
+  isVisible: boolean;
+}
+
+export enum TypoLength{
+  SHORT,
+  MEDIUM,
+  LONG
+}
+
+export enum TypoLanguage{
+  KOREAN,
+  ENGLISH
+}
+
+export enum TypoApprovalStatus{
+  WAITED,
+  APPROVED,
+  REJECTED
+}
+
+export interface ITypo{
+  articleNo: number;
+  author: IAuthor;
+  category:ICategory;
+  topic: ITopic;
+  title: string;
+  language: TypoLanguage;
+  writer: string;
+  publicationMedia: string;
+  publicationDate: string;
+  contents:string;
+  length: TypoLength,
+  approvalStatus: TypoApprovalStatus,
+  createAt: string;
+}
+
+export interface ITypoOption{
+  id :number;
+  email:string;
+  nickname:string;
+  profileImageUrl:string;
+  accessToken:string;
+  refreshToken:string;
+}
+
+export interface IFontOption{
+  id :number;
+  email:string;
+  nickname:string;
+  profileImageUrl:string;
+  accessToken:string;
+  refreshToken:string;
+}
