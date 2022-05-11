bf07a4a1bbf0ee3c68c04f1647f06fb5f51fb0c9
HongEunbeen <vvvv980@gmail.com>
2022-05-11 12:55:14 +0900

    feat : 글감 관련 api 구성

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,9 +1,17 @@
 
-import { ICategory, ITopic, ITypo } from 'utils/types';
+import { TlsOptions } from 'tls';
+import { ICategory, ITopic, ITypo, TypoLanguage, TypoLength } from 'utils/types';
 import instance from './instance';
 
-export const getArticle = (): Promise<{data: ITypo}> => 
-  instance.get(`/articles/main`);
+interface GetArticleParams {
+  languages : TypoLanguage[];
+  categoryCodes :ICategory[];
+  topicCodes : ITopic[];
+  lengths : TypoLength[];
+}
+
+export const getArticle = (params? : GetArticleParams): Promise<{data: ITypo}> => 
+  instance.get(`/articles/main?languages=${params.languages.toString()}&categoryCodes=${params.categoryCodes.toString()}&topicCodes=${params.topicCodes.toString()}&lengths=${params.lengths.toString()}`);
 
 export const getCategories = (): Promise<{data:ICategory[]}> => 
   instance.get(`/categories`);
--- src/api/typo.ts
+++ src/api/typo.ts
@@ -1,17 +1,37 @@
+import { ICategory, ITopic, ITypo, TypoApprovalStatus, TypoLanguage, TypoLength } from "utils/types";
 import instance from "./instance";
 
 export interface ITypoFilter{
-
+  languages: TypoLanguage;
+  categoryCodes: ICategory;
+  topicCodes: ITopic;
+  lengths: TypoLength;
 }
 
-export interface PostTypoFilterParams {
-  email : string;
-  password: string;
-  nickname: string;
+export interface PutTypoFilterParams {
+  languages : TypoLanguage[];
+  categoryCodes :ICategory[];
+  topicCodes : ITopic[];
+  lengths : TypoLength[];
+}
+
+export interface PostTypoParams{
+  category: number;
+  topic: number;
+  title: string;
+  language: TypoLanguage;
+  writer: string;
+  publicationMedia: string;
+  publicationDate: string;
+  contents: string;
+  approvalStatus: TypoApprovalStatus;
 }
 
 export const getUserTypoFilter = (): Promise<{ data: ITypoFilter }> =>
   instance.get('/users/conditional');
 
-export const putUserTypoFilter = (params: PostTypoFilterParams): Promise<{ data: ITypoFilter }> =>
+export const putUserTypoFilter = (params: PutTypoFilterParams): Promise<{ data: ITypoFilter }> =>
   instance.put('/users/conditional', params);
+
+export const postTypo = (params: PostTypoParams) : Promise<{data:ITypo}> => 
+  instance.post('/articles', params);
