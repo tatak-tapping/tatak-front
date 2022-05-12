ac9e150b15471b185abba22734facea7a8bd4f15
HongEunbeen <vvvv980@gmail.com>
2022-05-12 14:51:50 +0900

    feat : input props 수정 - as-is  : input html 요소를 export시 편리하게 하기 위해 type를 사용하고 따로 라이브러리의 타입을 수정해 사용 - to-be : props 인터페이스에서 해당 타입 상속받아 props 설정 + ref 따로 export시 설정

--- src/components/atoms/input/Input.tsx
+++ src/components/atoms/input/Input.tsx
@@ -1,22 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import styled from "@emotion/styled";
-import { useRef } from "react";
+import { ChangeEventHandler, forwardRef, useRef } from "react";
 import { BASE, ERROR, GRAY, PRIMARY } from "styles/colors";
 import InputBase from "./InputBase";
 import { useFormContext } from "react-hook-form";
 import { useEffect } from "react";
 import { IFormInputs } from "components/organisms/modals/SignUpModalContent";
-import { useController, UseControllerProps } from "react-hook-form";
+import { useController, Controller, UseControllerProps } from "react-hook-form";
 
-type InputProps = {
-  props:UseControllerProps<IFormInputs>;
-  type:string;
-  placeholder?:string;
-  counter?:string;
-  comment?:string;
-  onChange?:Function;
-  icon?: React.ReactNode
-} & React.InputHTMLAttributes<HTMLInputElement>
+interface InputProps extends UseControllerProps<IFormInputs>{
+   type?:string;
+   comment?:string;
+   counter?:string;
+   icon?:React.ReactElement;
+   placeholder?:string;
+}
 
 const StyledInput = styled.input` 
   width: 320px;
@@ -44,25 +42,33 @@ const Wrapper = styled.div<{error?:boolean}>`
   }
 `;
 
-function Input(props:UseControllerProps<IFormInputs>) {
-  const {field, fieldState} = useController(props);
-  
+function Input(props:InputProps, ref: React.Ref<HTMLDivElement>) {
+  const name = props.name;
+  const control = props.control;
+  const {field, fieldState} = useController({
+    name,
+    control,
+    rules: props.rules,
+    defaultValue: "",
+  });
+
   return (
     <InputBase 
-      comment={fieldState.error ? fieldState.error.message : props.comment} 
-      error={fieldState.error && true}
-      counter={props.counter}
-      >
-      <Wrapper error={fieldState.error && true}>
-         <StyledInput
-            {...field}
-            type={props.type}
-            placeholder={props.placeholder}
-          />
-        {props.icon}
-      </Wrapper>
-    </InputBase>
+        comment={fieldState.error ? fieldState.error.message : props.comment} 
+        error={fieldState.error && true}
+        counter={props.counter}
+        >
+        <div ref={ref}></div>
+        <Wrapper error={fieldState.error && true}>
+           <StyledInput
+              {...field}
+              type={props.type}
+              placeholder={props.placeholder}
+            />
+          {props.icon}
+        </Wrapper>
+      </InputBase>
   );
 };
 
-export default Input;
+export default forwardRef<HTMLDivElement, InputProps>(Input);
--- src/components/organisms/modals/SignUpModalContent.tsx
+++ src/components/organisms/modals/SignUpModalContent.tsx
@@ -1,19 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import {postCommonLogin, postUser } from "api/auth";
+import { postUser } from "api/auth";
 import TextButton from "components/atoms/button/TextButton";
 import { EyeOffIcon, EyeOnIcon, ReturnIcon } from "components/atoms/icon/Icon";
 import Input from "components/atoms/input/Input";
-import { InputCommontType } from "components/atoms/input/InputBase";
 import Label from "components/atoms/label/Label";
-import KakaoAccountButton from "components/molecules/button/KakaoAccountButton";
 import { useDialog } from "context/Dialog";
-import { useEffect } from "react";
-import { useCallback, useState } from "react";
-import { Box, Button, Flex, Link, Text } from "rebass";
+import { useEffect, useState } from "react";
+import { Box, Button, Flex , Text } from "rebass";
 import { BASE, GRAY, PRIMARY } from "styles/colors";
-import { IUser } from "utils/types";
-import { useForm, FormProvider } from "react-hook-form";
+import { useForm, useWatch, FormProvider } from "react-hook-form";
 import { DialogTypes } from "components/atoms/dialog/Dialog";
 import { setSessionStorage } from "utils/storage";
 import { useSetRecoilState } from "recoil";
@@ -25,7 +21,7 @@ interface SignModalContentProps {
   onClickLoginButton : () => void;
 }
 
-interface IFormInputs {
+export interface IFormInputs {
   email:string;
   nickname:string;
   password:string;
@@ -38,17 +34,13 @@ const SignUpModalContent = ({ onClickLoginButton } : SignModalContentProps) => {
   const navigate = useNavigate();
   const [isPwdConfirmVisible, setIsPwdConfirmVisible] = useState(false);
   const [isPwdVisible, setIsPwdVisible] = useState(false);
+
   const [isComplete, setIsComplete] = useState(false);
   const setUserToken = useSetRecoilState(tokenAtom);
   const setUser = useSetRecoilState(userAtom);
 
-  const handleReturnNickname = () => {
-    const date = new Date();
-    //setNickname(`타닥이${date.getSeconds()}${date.getMilliseconds()}`);
-  }
-
+  
   const onSubmit = async(data:IFormInputs) => {
-    console.log(data);
     const params = data;
     try {
       const { data } = await postUser(params);
@@ -57,11 +49,10 @@ const SignUpModalContent = ({ onClickLoginButton } : SignModalContentProps) => {
       instance.defaults.headers.common['Authorization'] = `Bearer ${data.accessToken}`;
       setUserToken(data.accessToken);
       setUser(data);
-
       navigate('/');
-      
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error.status);
+
       showDialog({
         type : DialogTypes.error,
         message : (
@@ -69,14 +60,7 @@ const SignUpModalContent = ({ onClickLoginButton } : SignModalContentProps) => {
             <Text>
               회원정보가 일치하지 않아요. 
             </Text>
-            <Button
-              onClick={closeDialog} 
-              width="76px" 
-              height="43px"
-              fontSize="16px"
-              
-              backgroundColor={PRIMARY[80]}
-              margin="20px">
+            <Button onClick={closeDialog} width="76px" height="43px" fontSize="16px" backgroundColor={PRIMARY[80]} margin="20px">
               확인
             </Button>
           </>
@@ -85,58 +69,99 @@ const SignUpModalContent = ({ onClickLoginButton } : SignModalContentProps) => {
     }
   }
 
-  const methods = useForm<IFormInputs>();
+  const methods = useForm<IFormInputs>({
+    defaultValues:{
+      email : "",
+      nickname : "",
+      password : "",
+      passwordConfirm : ""
+    },
+    mode:"onChange"
+  });
+
   const {
-    register,
-    watch,
+    control,
     setError,
-    formState: { errors },
+    formState: {isSubmitting, isDirty, isValid, errors},
+    setValue,
+    watch,
     handleSubmit
   } = methods;
 
+  const handleReturnNickname = () => {
+    const date = new Date();
+    setValue("nickname", `타닥이${date.getSeconds()}${date.getMilliseconds()}`);
+  }
+
+  const nickNameWatch = useWatch({control, name: "nickname"});
+  const passwordWatch = useWatch({control, name: "password"});
+  
   return(
     <Flex flexDirection="column" ml={20} mr={20}>
       <Box>
         <Text fontSize="24px" color={GRAY[2]} lineHeight="150%" fontWeight="600">
           회원가입
         </Text>
-        <hr css={css`
-          width: 100%;
-          border : none;
-          border-top: 1px solid ${GRAY[2]};
-        `}/>
+        <hr css={css`width: 100%; border : none; border-top: 1px solid ${GRAY[2]};`}/>
       </Box>
       <FormProvider {...methods} >
         <form onSubmit={handleSubmit(onSubmit)}>
           <Box mt={20}>
             <Label>E-mail</Label>
-            <Input 
-              {...register("email")}
-              type="email"
+            <Input
               name="email"
-              placeholder="이메일을 입력해주세요."/>
+              rules={{
+                required:true,
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: "정확한 이메일을 입력해주세요."
+                }
+              }}
+              control={control}
+              type="text"
+              placeholder="이메일을 입력해주세요."
+              />
           </Box>
           <Box mt={20}>
             <Label>닉네임</Label>
-            <Input 
-              {...register("nickname")}
+            <Input
+              name="nickname"
+              rules={{
+                required:true, 
+                minLength:{
+                  value:2,
+                  message:'최소 2자, 최대 10자까지 가능해요.'
+                },
+                maxLength:{
+                  value:10, 
+                  message:'최소 2자, 최대 10자까지 가능해요.'
+                }
+              }}
+              control={control}
               type="text"
+              counter={nickNameWatch.length + "/10"}
               placeholder="닉네임을 입력해주세요."
               comment="최소 2자, 최대 10자까지 가능해요."
-              name="nickname"
               icon={
                 <span onClick={() => handleReturnNickname()}>
                   <ReturnIcon />
                 </span>
-              }/>
+              }
+             />
           </Box>
           <Box mt={20}>
             <Label>비밀번호</Label>
             <Input
-              {...register("password")}
               name="password"
               type={isPwdVisible ? "text" : "password"}
-              comment={errors.password && '최소 6자리로 입력해주세요.'}
+              rules={{
+                required:true, 
+                minLength:{
+                  value:6,
+                  message:'최소 6자리로 입력해주세요.'
+                }}
+              }
+              control={control}
               placeholder="비밀번호를 입력해주세요."
               icon={isPwdVisible ?
                 (<span onClick={() => setIsPwdVisible(!isPwdVisible)}>
@@ -146,14 +171,25 @@ const SignUpModalContent = ({ onClickLoginButton } : SignModalContentProps) => {
                 (<span onClick={() => setIsPwdVisible(!isPwdVisible)}>
                     <EyeOffIcon/>
                   </span>)
-              } />
+              } 
+            />
           </Box>
           <Box mt={20}>
             <Label>비밀번호 확인</Label>
-            <Input 
+            <Input
               name="passwordConfirm"
-              type={isPwdConfirmVisible ? "text" : "password"} 
-              comment={errors?.passwordConfirm?.message}
+              rules={{
+                required:true, 
+                minLength:{
+                  value:6,
+                  message:'최소 6자리로 입력해주세요.'
+                },
+                validate: (value) => {
+                  if (value !== passwordWatch) return "비밀번호가 일치하지 않아요.";
+                }
+              }}
+              control={control}
+              type={isPwdConfirmVisible ? "text" : "password"}
               placeholder="다시 한 번 비밀번호를 입력해주세요."
               icon={isPwdConfirmVisible ?
                 (<span onClick={() => setIsPwdConfirmVisible(!isPwdConfirmVisible)}>
@@ -163,28 +199,23 @@ const SignUpModalContent = ({ onClickLoginButton } : SignModalContentProps) => {
                 (<span onClick={() => setIsPwdConfirmVisible(!isPwdConfirmVisible)}>
                     <EyeOffIcon/>
                   </span>)
-              } />
+              }/>
           </Box>
-              <TextButton
-                width="348px"
-                height="43px"
-                fontSize="16px"
-                fontColor={BASE[3]}
-                backgroundColor={PRIMARY[80]}
-                disabled={!isComplete}
-              >
-                
-              </TextButton>
           <Box mt={28}>
-            <input type="submit" />
+            <TextButton
+              width="348px"
+              height="43px"
+              fontSize="16px"
+              fontColor={BASE[3]}
+              backgroundColor={PRIMARY[80]}
+              onClick={handleSubmit(onSubmit)}
+              disabled={!isDirty || !isValid}>
+                회원가입
+            </TextButton>
           </Box>
         </form>
       </FormProvider>
-      <Box mt={28}sx={{
-        borderTop : `1px solid ${GRAY[2]}`,
-        paddingTop: `16px`,
-        textAlign: `center`
-      }}>
+      <Box mt={28}sx={{borderTop : `1px solid ${GRAY[2]}`, paddingTop: `16px`, textAlign: `center`}}>
       <Text fontSize="12px" color={GRAY[5]}>
         이미 가입한 계정이 있으신가요?
       </Text>
