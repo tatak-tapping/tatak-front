d0d8918efd9b870ebe9d7cd35c84bbf4bce17beb
seonjeong <theblessedcity@hanmail.net>
2022-06-25 03:13:08 +0900

    feat : add click event to open img save

--- src/components/organisms/typo/TypoFooterSection.tsx
+++ src/components/organisms/typo/TypoFooterSection.tsx
@@ -1,4 +1,6 @@
 /** @jsxImportSource @emotion/react */
+import React from 'react';
+
 import { css } from '@emotion/react';
 import { getArticle } from 'api/common';
 import IconAndTextButton from 'components/atoms/button/IconAndTextButton';
@@ -9,10 +11,17 @@ import { useRecoilState } from 'recoil';
 import { GRAY } from 'styles/colors';
 import { ITypo } from 'utils/types';
 
+import useModal from 'hooks/userModal';
+import IconButton from 'components/atoms/button/IconButton';
+import { CloseIcon } from 'components/atoms/icon/Icon';
+import FeedDownModalContent from 'components/organisms/modals/feed/FeedDownModalContent';
+
 const TypoFooterSection = () => {
   const [typo, setTypo] = useRecoilState<ITypo>(typoAtom);
 
-  const handleClick = () => alert('준비중입니다.');
+  const handleClick = () => {
+    handleOpenModal();
+  };
 
   const handleRefreshClick = () => {
     const getArticleAsync = async () => {
@@ -22,79 +31,89 @@ const TypoFooterSection = () => {
     getArticleAsync();
   };
 
+  const { renderModal, handleOpenModal, handleCloseModal } = useModal({});
+
   return (
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      marginLeft="auto"
-      marginRight="auto"
-      width={1000}
-      height={52}
-      padding="0 24px"
-    >
-      <Flex>
-        <div
-          css={css`
-            width: 40px;
-            height: 40px;
-            background-color: white;
-            border-radius: 50%;
-            text-align: center;
-            line-height: 40px;
-          `}
-        >
-          {typo?.category.emoji}
-        </div>
-        <Flex flexDirection="column" ml="8px">
-          <Box>
-            <span
-              css={css`
-                font-weight: 600;
-                font-size: 14px;
-                line-height: 17px;
-                color: ${GRAY[3]};
-              `}
-            >
-              『{typo?.title}』
-            </span>
-            <span
-              css={css`
-                margin-left: 4px;
-                font-weight: 400;
-                font-size: 14px;
-                line-height: 17px;
-                color: ${GRAY[3]};
-              `}
-            >
-              {typo?.writer}
-            </span>
-          </Box>
-          <Box>
-            <div
-              css={css`
-                font-weight: 400;
-                font-size: 12px;
-                line-height: 14px;
-                color: ${GRAY[6]};
-              `}
-            >
-              {typo?.author.nickname}
-            </div>
-          </Box>
+    <>
+      {renderModal(
+        <FeedDownModalContent />,
+        <IconButton width="32px" height="32px" border="none" onClick={handleCloseModal}>
+          <CloseIcon />
+        </IconButton>
+      )}
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        marginLeft="auto"
+        marginRight="auto"
+        width={1000}
+        height={52}
+        padding="0 24px"
+      >
+        <Flex>
+          <div
+            css={css`
+              width: 40px;
+              height: 40px;
+              background-color: white;
+              border-radius: 50%;
+              text-align: center;
+              line-height: 40px;
+            `}
+          >
+            {typo?.category.emoji}
+          </div>
+          <Flex flexDirection="column" ml="8px">
+            <Box>
+              <span
+                css={css`
+                  font-weight: 600;
+                  font-size: 14px;
+                  line-height: 17px;
+                  color: ${GRAY[3]};
+                `}
+              >
+                『{typo?.title}』
+              </span>
+              <span
+                css={css`
+                  margin-left: 4px;
+                  font-weight: 400;
+                  font-size: 14px;
+                  line-height: 17px;
+                  color: ${GRAY[3]};
+                `}
+              >
+                {typo?.writer}
+              </span>
+            </Box>
+            <Box>
+              <div
+                css={css`
+                  font-weight: 400;
+                  font-size: 12px;
+                  line-height: 14px;
+                  color: ${GRAY[6]};
+                `}
+              >
+                {typo?.author.nickname}
+              </div>
+            </Box>
+          </Flex>
+        </Flex>
+        <Flex justifyContent="center" alignItems="center" marginLeft="auto">
+          <IconAndTextButton onClick={handleRefreshClick} icon={<ReturnIcon />}>
+            새로고침
+          </IconAndTextButton>
+          <IconAndTextButton onClick={handleClick} icon={<BookMarkIcon />}>
+            스크랩
+          </IconAndTextButton>
+          <IconAndTextButton onClick={handleClick} icon={<TurnUpIcon />}>
+            이미지 저장
+          </IconAndTextButton>
         </Flex>
       </Flex>
-      <Flex justifyContent="center" alignItems="center" marginLeft="auto">
-        <IconAndTextButton onClick={handleRefreshClick} icon={<ReturnIcon />}>
-          새로고침
-        </IconAndTextButton>
-        <IconAndTextButton onClick={handleClick} icon={<BookMarkIcon />}>
-          스크랩
-        </IconAndTextButton>
-        <IconAndTextButton onClick={handleClick} icon={<TurnUpIcon />}>
-          이미지 저장
-        </IconAndTextButton>
-      </Flex>
-    </Flex>
+    </>
   );
 };
 
