8738c038a7bae867f04d6c2e82c5f29dde5ef3ae
seonjeong <theblessedcity@hanmail.net>
2022-07-06 00:55:29 +0900

    fix : set middle align of typo section

--- src/components/templates/typo/TypoTemplate.tsx
+++ src/components/templates/typo/TypoTemplate.tsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled';
+
 import TypoFooterSection from 'components/organisms/typo/TypoFooterSection';
 import TypoMainSection from 'components/organisms/typo/TypoMainSection';
 import {
@@ -11,12 +13,16 @@ interface TypoTemplateProps {
   handleFullScreen?: FullScreenHandle;
 }
 
+const StyledTypo = styled.div`
+  margin-top: 60px;
+`;
+
 const TypoTemplate = ({ handleFullScreen }: TypoTemplateProps) => {
   return (
-    <>
+    <StyledTypo>
       <TypoMainSection />
       <TypoFooterSection />
-    </>
+    </StyledTypo>
   );
 };
 
