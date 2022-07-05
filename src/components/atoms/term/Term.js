ace25e052e5f1ce5e2bfc3e2b4a82f15f65f4f1b
seonjeong <theblessedcity@hanmail.net>
2022-07-05 23:59:18 +0900

    feat : add term component

---
+++ src/components/atoms/term/Term.js
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled';
+import { BASE, GRAY, PRIMARY } from 'styles/colors';
+
+export const StyledTermText = styled.p`
+  font-size: 15px;
+  line-height: 1.6;
+  color: #616161;
+  margin: 0;
+  margin-top: 24px;
+  background: transparent;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+export const StyledTermTitle1 = styled.h1`
+  color: #616161;
+  font-size: 16px;
+  line-height: 1.6;
+  font-weight: 700;
+  margin: 0;
+  margin-top: 20px;
+  background: transparent;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+export const StyledTermTitle2 = styled.h2`
+  color: #616161;
+  font-size: 14px;
+  line-height: 1.6;
+  font-weight: 600;
+  margin: 0;
+  margin-top: 16px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: transparent;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+export const StyledText = styled.p`
+  color: #616161;
+  font-size: 14px;
+  line-height: 1.6;
+  font-weight: 400;
+  margin: 0;
+  margin-top: 16px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: transparent;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+export const StyledTermBox = styled.div`
+  padding: 20px;
+  margin: 0;
+  margin-top: 24px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background: #f2f1ec;
+`;
